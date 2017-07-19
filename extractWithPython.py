34eb1ac818d5ac219ae0a765577064424232c0b4
thangbk2209 <thangbk2209@gmail.com>
2017-07-20 02:46:53 +0700

    fix

--- extractWithPython.py
+++ extractWithPython.py
@@ -3,14 +3,11 @@ import numpy as np
 import os
 from pandas import read_csv
 # file_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/results/out.csv'
-folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/TopJobId/'
-# folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
+folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
 # minTime = data[0][0]
 list_file_name = []
 list_max_time = []
-
-for num in range(175,271):
-    file_name = "JobMaxTaskpart-00"+str(num).zfill(3)+"-of-00500.csv"
+for file_name in os.listdir(folder_path):
 	list_file_name.append(file_name)
 	print len(list_file_name)
 	df = read_csv('%s%s'%(folder_path,file_name), header=None,index_col=False)
@@ -20,6 +17,8 @@ for num in range(175,271):
 		if(data[i][1] > maxTime):
 			maxTime = data[i][1]
 	list_max_time.append(maxTime)
+	if(len(list_file_name) == 100):
+		break
 print "List file name: "
 print list_file_name
 print "List max time: "
--- extractWithSpark.py
+++ extractWithSpark.py
@@ -1,8 +1,4 @@
-# Lay ra resource usage tai cac diem thoi gian- chi lay ra cac ban ghi thoa man rang start time < time stamp
-# va end time > time stamp ma chua cong lai cac gia tri resource cua no
-# y tuong: Tim ra min start time vaf max end time trong tung part. Sau do truy van du lieu trong tung part 
-#voi gia tri tang dan tu gia tri min start time cua part do- tuy nhien chua xu li van de chong lan thoi gian
-# giua cac part -- max end time cua part truoc > min start cuar part sau
+# Truy xuat ra min start time va max end time cuar tat ca cac part trong bo du lieu google cluster trace
 from pyspark.sql.session import SparkSession as spark
 import pandas as pd
 from pyspark import SparkContext
@@ -13,12 +9,14 @@ import os
 sc = SparkContext(appName="Task_usage")
 sql_context = SQLContext(sc)
 
-# folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
-folder_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/TopJobId/'
+folder_path ='/mnt/volume/ggcluster/clusterdata-2011-2/task_usage/'
+# file_path = '/mnt/volume/ggcluster/spark-2.1.1-bin-hadoop2.7/thangbk2209/results/out.csv'
 
-dataSchema = StructType([StructField('startTime', StringType(), True),
-                         StructField('endTime', StringType(), True),
+dataSchema = StructType([StructField('startTime', LongType(), True),
+                         StructField('endTime', LongType(), True),
                          StructField('JobId', LongType(), True),
+                         StructField('taskIndex', LongType(), True),
+                         StructField('machineId', LongType(), True),
                          StructField('meanCPUUsage', FloatType(), True),
                          # canonical memory usage
                          StructField('CMU', FloatType(), True),
@@ -27,13 +25,23 @@ dataSchema = StructType([StructField('startTime', StringType(), True),
                          # unmapped page cache memory usage
                          StructField('unmapped_cache_usage', FloatType(), True),
                          StructField('page_cache_usage', FloatType(), True),
+                         StructField('max_mem_usage', FloatType(), True),
                          StructField('mean_diskIO_time', FloatType(), True),
-                         StructField('mean_local_disk_space', FloatType(), True)])
-FileNameARR=[]
+                         StructField('mean_local_disk_space', FloatType(), True),
+                         StructField('max_cpu_usage', FloatType(), True),
+                         StructField('max_disk_io_time', FloatType(), True),
+                         StructField('cpi', FloatType(), True),
+                         StructField('mai', FloatType(), True),
+                         StructField('sampling_portion', FloatType(), True),
+                         StructField('agg_type', FloatType(), True),
+                         StructField('sampled_cpu_usage', FloatType(), True)])
 
-for num in range(175,271):
-    file_name = "JobMaxTaskpart-00"+str(num).zfill(3)+"-of-00500.csv"
-    FileNameARR.append(file_name)
+list_file_name=[]
+list_max_time=[]
+# 2505600000000
+for file_name in os.listdir(folder_path):
+    list_file_name.append(file_name)
+    print len(list_file_name)
     df = (
         sql_context.read
         .format('com.databricks.spark.csv')
@@ -41,9 +49,15 @@ for num in range(175,271):
         .load("%s%s"%(folder_path,file_name))
     )
     df.createOrReplaceTempView("dataFrame")
-   
-    TimeDf = sql_context.sql("SELECT min(startTime),max(endTime) from dataFrame")
-   
-    TimeDf.toPandas().to_csv('thangbk2209/sparkvspython/Data/sparkData/Time%s'%(file_name), index=False, header=None)
-print FileNameARR
+    maxEnd = sql_context.sql("SELECT max(endTime) as maxEndTime from dataFrame").rdd.map(lambda r: r.maxEndTime).collect()
+    # minStart = sql_context.sql("SELECT min(startTime) as minStartTime from dataFrame").rdd.map(lambda r: r.minStartTime).collect()
+    maxTime = int(maxEnd[0])
+    list_max_time.append(maxTime)
+    if(len(list_file_name)==100):
+        break
+    # minTime = int(minStart[0])
+print "List file name: "
+print list_file_name
+print "List max time: "
+print list_max_time
 sc.stop()
